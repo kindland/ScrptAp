66582f99d58169091e4d7f14d1239cdd14ad8b93
Vitc <cmolosit@gmail.com>
2025-02-23 16:16:31 +0200

    Add files via upload

---
+++ LibraryBase.cs
@@ -0,0 +1,313 @@
+/* This content is licensed under the terms of the Creative Commons Attribution 4.0 International License.
+ * When using this content, you must:
+ * •    Acknowledge that the content is from the Sansar Knowledge Base.
+ * •    Include our copyright notice: "© 2022 Sansar, Inc."
+ * •    Indicate that the content is licensed under the Creative Commons Attribution-Share Alike 4.0 International License.
+ * •    Include the URL for, or link to, the license summary at https://creativecommons.org/licenses/by-sa/4.0/deed.hi (and, if possible, to the complete license terms at https://creativecommons.org/licenses/by-sa/4.0/legalcode.
+ * For example:
+ * "This work uses content from the Sansar Knowledge Base. © 2022 Sansar, Inc. Licensed under the Creative Commons Attribution 4.0 International License (license summary available at https://creativecommons.org/licenses/by/4.0/ and complete license terms available at https://creativecommons.org/licenses/by/4.0/legalcode)."
+ */
+
+using Sansar.Script;
+using Sansar.Simulation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+[assembly: Tooltip("A collection of scripts that work together through named events to do common simple scripting tasks.\nEvents that start with -> respond to event names by doing something.\nEvents that end with -> will send an event after something is done.")]
+namespace ScriptLibrary 
+{
+    // Adding any data to this will mean messages from old scripts will fail to be processed correctly in new scripts.
+    public interface ISimpleData
+    {
+        AgentInfo AgentInfo { get; }
+        ObjectId ObjectId { get; }
+        ObjectId SourceObjectId { get; }
+
+        // Extra data
+        Reflective ExtraData { get; }
+    }
+
+    public class SimpleData : Reflective, ISimpleData
+    {
+        public SimpleData(ScriptBase script) { ExtraData = script; }
+        public AgentInfo AgentInfo { get; set; }
+        public ObjectId ObjectId { get; set; }
+        public ObjectId SourceObjectId { get; set; }
+
+        public Reflective ExtraData { get; }
+    }
+
+    internal class EventCollection
+    {
+        internal EventCollection(IEnumerable<string> eventChain, IBase script)
+        {
+            if (eventChain.Count() == 0) return;
+
+            Events = eventChain.First().Trim().Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+            for (int i = 0; i < Events.Length; ++i)
+            {
+                Events[i] = GenerateEventName(Events[i], script.GetGroup(), script.GetObjectId());
+            }
+
+            if (eventChain.Count() > 1)
+            {
+                Next = new EventCollection(eventChain.Skip(1), script);
+            }
+        }
+
+        private string GenerateEventName(string eventName, string group, ObjectId objectId)
+        {
+            eventName = eventName.Trim();
+            if (eventName.EndsWith("@"))
+            {
+                // Special case on@ to send the event globally (the null group) by sending w/o the @.
+                eventName = eventName.Substring(0, eventName.Length - 1);
+            }
+            else if (group != "" && !eventName.Contains("@"))
+            {
+                // Append the group if there is a group and there isn't a group override set
+                eventName = eventName + "@" + group;
+            }
+
+            return eventName.Replace("{object}", "{o:" + objectId + "}");
+        }
+
+        internal string[] Events;
+        internal EventCollection Next = null;
+        internal Action Unsubscribe = null;
+    }
+
+    internal class EventWrapper
+    {
+        internal EventWrapper(string eventCsv, IBase script)
+        {
+            ThisScript = script;
+            Head = new EventCollection(eventCsv.Split(new string[] { ">>" }, StringSplitOptions.RemoveEmptyEntries), script);
+        }
+
+        internal void SendToAll(Reflective data)
+        {
+            Next();
+
+            ThisScript.SimpleLog(LogLevel.Info, "Sending " + Current.Events.Length + " events: " + string.Join(", ", Current.Events));
+
+            foreach (string eventName in Current.Events)
+            {
+                ThisScript.PostSimpleEvent(eventName, data);
+            }
+        }
+
+        internal Action SubscribeToAll(Action<ScriptEventData> callback)
+        {
+            Next();
+
+            ThisScript.SimpleLog(LogLevel.Info, "Subscribing to " + Current.Events.Length + " events: " + string.Join(", ", Current.Events));
+            
+            Action<ScriptEventData> wrappedCallback = callback;
+            foreach (string eventName in Current.Events)
+            {
+                if (ThisScript.DebugEnabled())
+                {
+                    var sub = ThisScript.SubscribeToSimpleEvent(eventName, (ScriptEventData data) =>
+                    {
+                        ThisScript.SimpleLog(LogLevel.Info, "Received event " + eventName);
+                        wrappedCallback(data);
+                    });
+                    Current.Unsubscribe += sub.Unsubscribe;
+                }
+                else
+                {
+                    var sub = ThisScript.SubscribeToSimpleEvent(eventName, wrappedCallback);
+                    Current.Unsubscribe += sub.Unsubscribe;
+                }
+            }
+            Current.Unsubscribe += () => ThisScript.UnsubscribeToSimpleEvent(Current.Events.Length);
+            return Current.Unsubscribe;
+        }
+
+        public void Next()
+        {
+            if (Current != null && Current.Unsubscribe != null)
+            {
+                Current.Unsubscribe();
+                Current.Unsubscribe = null;
+            }
+
+            if (Current == null) Current = Head;
+            else if (Current.Next != null) Current = Current.Next;
+            else if (Current != Head) Current = Head;
+        }
+
+        internal void Reset()
+        {
+            if (Current != null && Current.Unsubscribe != null)
+            {
+                Current.Unsubscribe();
+                Current.Unsubscribe = null;
+            }
+
+            Current = null;
+        }
+
+        private readonly IBase ThisScript = null;
+        private readonly EventCollection Head = null;
+        private EventCollection Current = null;
+    }
+
+    internal interface IBase
+    {
+        void SimpleLog(LogLevel level, string logMessage);
+        string GetGroup();
+        ObjectId GetObjectId();
+        bool DebugEnabled();
+
+        void PostSimpleEvent(string name, Reflective data);
+
+        IEventSubscription SubscribeToSimpleEvent(string Message, Action<ScriptEventData> callback);
+        void UnsubscribeToSimpleEvent(int eventCount);
+    }
+
+    public interface IDebug
+    {
+        bool DebugSimple { get; }
+        void TrackSubscribe(string name, int count = 1);
+        bool TrackingSubscriptions { get; }
+    }
+
+    public class DebugDisabled : IDebug
+    {
+        public bool DebugSimple { get { return false; } }
+        public void TrackSubscribe(string name, int count = 1) { }
+        public bool TrackingSubscriptions { get { return false; } }
+    }
+
+    public abstract class LibraryBase : SceneObjectScript, IBase
+    {
+        [Tooltip(@"If a Group is set, will only respond and send to other SimpleScripts with the same Group tag set.
+Does NOT accept CSV lists of groups.
+Use {object} as the group to create a group that is unique to this object.
+To send or receive events to/from a specific group from outside that group append an @ and the group name to the event name, e.g. ""on@my_group""")]
+        [DefaultValue("")]
+        [DisplayName("Group")]
+        public string Group = "";
+
+        [DisplayName("Fast Start")]
+        [Tooltip("Fast Start\nEnabling Fast Start will skip waiting for debugger initialization. This means debug logs from early in the script's initialization may be missed or lost.\n\nWARNING: Enable only if the 1.5s delay at initialization when no debugger is in the scene is really killing you.")]
+        [DefaultValue(false)]
+        public bool FastStart = false;
+
+        internal IDebug __SimpleDebugger = new DebugDisabled();
+        internal string __SimpleTag = "";
+
+        #region IBase
+        public void SimpleLog(LogLevel level, string logMessage)
+        {
+            if (level == LogLevel.Error || __SimpleDebugger.DebugSimple) Log.Write(level, __SimpleTag, logMessage);
+        }
+        public string GetGroup() { return Group; }
+        public ObjectId GetObjectId() { return ObjectPrivate.ObjectId; }
+        public bool DebugEnabled() { return __SimpleDebugger.DebugSimple; }
+
+        public void PostSimpleEvent(string name, Reflective data)
+        {
+            PostScriptEvent(name, data);
+        }
+
+        public IEventSubscription SubscribeToSimpleEvent(string Message, Action<ScriptEventData> callback)
+        {
+            if (__SimpleDebugger.TrackingSubscriptions) __SimpleDebugger.TrackSubscribe(ObjectPrivate.Name);
+            return base.SubscribeToScriptEvent(Message, callback);
+        }
+
+        public void UnsubscribeToSimpleEvent(int eventCount)
+        {
+            if (__SimpleDebugger.TrackingSubscriptions) __SimpleDebugger.TrackSubscribe(ObjectPrivate.Name, eventCount * -1);
+        }
+        #endregion
+
+        protected abstract void SimpleInit();
+        bool foundDebugger = false;
+
+        public sealed override void Init()
+        {
+            __SimpleTag = GetType().Name + " [S:" + Script.ID.ToString() + " O:" + ObjectPrivate.ObjectId.ToString() + "]";
+            StartCoroutine(FindDebugger, 0.1);
+
+            if (!FastStart)
+            {
+                Yield();
+                int retries = 15;
+                while (!foundDebugger && --retries > 0) Wait(0.1);
+            }
+
+            SimpleInit();
+        }
+
+        private void FindDebugger(double rate)
+        {
+            int retries = 10;
+            IDebug debugger = null;
+            do
+            {
+                debugger = ScenePrivate.FindReflective<IDebug>("Simple.Debugger").FirstOrDefault();
+                if (debugger == null) Wait(rate);
+            } while (debugger == null && --retries > 0);
+
+            if (debugger != null)
+            {
+                __SimpleDebugger = debugger;
+                return;
+            }
+
+            if (rate < 3.0)
+            {
+                StartCoroutine(() =>
+                {
+                    FindDebugger(rate * 2);
+                });
+            }
+        }
+
+        private Dictionary<string, EventWrapper> SendEventWrappers = new Dictionary<string, EventWrapper>();
+        private Dictionary<string, EventWrapper> SubscribeEventWrappers = new Dictionary<string, EventWrapper>();
+
+        private EventWrapper GetWrapper(Dictionary<string, EventWrapper> eventWrappers, string events)
+        {
+            if (string.IsNullOrWhiteSpace(events)) return null;
+
+            EventWrapper wrapper;
+            if (!eventWrappers.TryGetValue(events, out wrapper))
+            {
+                wrapper = new EventWrapper(events, this);
+                eventWrappers[events] = wrapper;
+            }
+            return wrapper;
+        }
+
+        protected void SendToAll(string csv, Reflective data)
+        {
+            GetWrapper(SendEventWrappers, csv)?.SendToAll(data);
+        }
+
+        protected Action SubscribeToAll(string csv, Action<ScriptEventData> callback)
+        {
+            return GetWrapper(SubscribeEventWrappers, csv)?.SubscribeToAll(callback);
+        }
+
+        protected void ResetSendState(string csv)
+        {
+            GetWrapper(SendEventWrappers, csv)?.Reset();
+        }
+
+        protected void NextSendState(string csv)
+        {
+            GetWrapper(SendEventWrappers, csv)?.Next();
+        }
+
+        protected void ResetSubscribeState(string csv)
+        {
+            GetWrapper(SubscribeEventWrappers, csv)?.Reset();
+        }
+    }
+}
---
+++ LibraryDebugger.cs
@@ -0,0 +1,133 @@
+/* This content is licensed under the terms of the Creative Commons Attribution 4.0 International License.
+ * When using this content, you must:
+ * �    Acknowledge that the content is from the Sansar Knowledge Base.
+ * �    Include our copyright notice: "� 2022 Sansar, Inc."
+ * �    Indicate that the content is licensed under the Creative Commons Attribution-Share Alike 4.0 International License.
+ * �    Include the URL for, or link to, the license summary at https://creativecommons.org/licenses/by-sa/4.0/deed.hi (and, if possible, to the complete license terms at https://creativecommons.org/licenses/by-sa/4.0/legalcode.
+ * For example:
+ * "This work uses content from the Sansar Knowledge Base. � 2022 Sansar, Inc. Licensed under the Creative Commons Attribution 4.0 International License (license summary available at https://creativecommons.org/licenses/by/4.0/ and complete license terms available at https://creativecommons.org/licenses/by/4.0/legalcode)."
+ */
+
+using Sansar.Script;
+using Sansar.Simulation;
+using System;
+using System.Collections.Generic;
+
+namespace ScriptLibrary
+{
+    [Tooltip("Add this anywhere to see all simple script activity in the debug console. (CTRL+D)")]
+    [DisplayName(nameof(Debugger))]
+
+    // This class is used to debug simple scripts
+    public class Debugger : LibraryBase, IDebug
+    {
+        #region EditorProperties
+        [Tooltip("If set all library scripts in the scene will log the events they receive and send to the script debug console.")]
+        [DefaultValue(true)]
+        [DisplayName("Debug Events")]
+        public readonly bool _DebugEvents = true;
+
+        [Tooltip("If set will regularly report memory use to the script debug console.\nWhenever the memory used changes by more than 10% since the last report it will be logged.")]
+        [DefaultValue(false)]
+        [DisplayName("Debug Memory")]
+        public readonly bool DebugMemoryEnabled;
+
+        [Tooltip("-> Memory Report\nSends a report to the script debug console with current memory usage when the event is received.")]
+        [DefaultValue("")]
+        [DisplayName("-> Memory Report")]
+        public readonly string MemoryReportEvent;
+
+        [Tooltip("-> Subscription Report\nSends a report to the script debug console with information about script event subscriptions."
+            + "\nNote: may not track subscriptions from older library scripts, or non-library scripts.")]
+        [DefaultValue("")]
+        [DisplayName("-> Subscription Report")]
+        public readonly string SubscriptionReportEvent;
+        #endregion
+
+        public bool DebugSimple { get { return _DebugEvents; } }
+
+        protected override Context ReflectiveContexts => Context.ScenePrivate | Context.ScenePublic;
+        protected override string ReflectiveName => "Simple.Debugger";
+        bool TrackSubscriptionsEnabled = false;
+
+        protected override void SimpleInit()
+        {
+            TrackSubscriptionsEnabled = !string.IsNullOrWhiteSpace(SubscriptionReportEvent);
+
+            if (DebugMemoryEnabled) StartCoroutine(DebugMemory);
+
+            SubscribeToAll(MemoryReportEvent, MemoryReport);
+            SubscribeToAll(SubscriptionReportEvent, SubscribeReport);
+        }
+
+        LogLevel GetLogLevel()
+        {
+            if (Memory.UsedBytes > Memory.PolicyWarning) return LogLevel.Warning;
+            else if (Memory.UsedBytes > Memory.PolicyLimit) return LogLevel.Error;
+            return LogLevel.Info;
+        }
+
+        void DebugMemory()
+        {
+            uint usedBytes = Memory.UsedBytes;
+
+            while (true)
+            {
+                if (usedBytes == 0)
+                {
+                    usedBytes = Memory.UsedBytes;
+                    if (usedBytes != 0)
+                    {
+                        Log.Write(GetLogLevel(), "MemoryDebug", $"Current memory used is {(int)(Memory.UsedBytes / 1024)}kb, peak memory used is {(int)(Memory.PeakUsedBytes / 1024)}kb");
+                    }
+                }
+                else if (Math.Abs(Memory.UsedBytes - usedBytes) > (usedBytes * 0.1))
+                {
+                    Log.Write(GetLogLevel(), "MemoryDebug", $"Current memory used is {(int)(Memory.UsedBytes / 1024)}kb, peak memory used is {(int)(Memory.PeakUsedBytes / 1024)}kb");
+                    usedBytes = Memory.UsedBytes;
+                    Wait(4); // Wait extra time after logging to avoid flooding on rapid changes.
+                }
+                Wait(1);
+            }
+        }
+
+        void MemoryReport(ScriptEventData data)
+        {
+            Log.Write(GetLogLevel(), "MemoryReport", $"Current memory used is {(int)(Memory.UsedBytes / 1024)}kb, peak memory used is {(int)(Memory.PeakUsedBytes / 1024)}kb");
+        }
+
+        public bool TrackingSubscriptions {  get { return TrackSubscriptionsEnabled; } }
+
+        Dictionary<string, int> Subscriptions = new Dictionary<string, int>();
+        public void TrackSubscribe(string name, int count = 1)
+        {
+            if (!TrackSubscriptionsEnabled) return;
+
+            if (Subscriptions.ContainsKey(name))
+            {
+                count += Subscriptions[name];
+            }
+
+            if (count > 0)
+            {
+                Subscriptions[name] = count;
+            }
+            else
+            {
+                Subscriptions.Remove(name);
+            }
+        }
+
+        public void SubscribeReport(ScriptEventData data)
+        {
+            int total = 0;
+            Log.Write(LogLevel.Info, "EventReport", "ScriptEvent Subscriptions by Object:");
+            foreach(var sub in Subscriptions)
+            {
+                Log.Write(LogLevel.Info, "EventReport", "   " + sub.Key + " has " + sub.Value + " subscriptions.");
+                total += sub.Value;
+            }
+            Log.Write(LogLevel.Info, "EventReport", "Total subscriptions is " + total);
+        }
+    }
+}
